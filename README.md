d1294cf279231877820072b3b5cf6c4810cfb3df
Mike van den Brink <mbrink1999@outlook.com>
2020-11-03 19:05:29 +0100

    Multiple clients and servers

--- README.md
+++ README.md
@@ -4,10 +4,32 @@ Response at route '/':
 ```
 {
 	"Code": 200,
-	"Data": {
-		"Private": "uBd9sMLggTWS8IzABu+dbx+DU5ntbH0NpHxlS8gcT2k=",
-		"Public": "WqLWgc5lCXxDMZc1AIjaFrSrl7gALMHnIaLJSSvjs0U="
-	},
-	"Message": "Wireguard key generated"
+	"Data": [{
+		"Private": "WOjaHmy3d8ij11ldF/13bAe1amidgfcsr2QnR9gQc0k=",
+		"Public": "SYGR76Alumi3ZvW+ZQ6X2W32npjiWjCOXhwFzgB7uTw=",
+		"Server": 0,
+		"Type": "Server"
+	}, {
+		"Private": "qKfiGCPvE+Jm8JH3abuqts9QAX0ZuzTzCRlmFLdtWUQ=",
+		"Public": "W56xKYVsVDmt/lFlJUXDpi+6S9ySpp0XJCnViI4ldQU=",
+		"Server": 1,
+		"Type": "Server"
+	}, {
+		"Client": 0,
+		"Private": "INm+8jMuV6JfiLjhV7xNMDty7/I4GBbcKfmNMTg2BHQ=",
+		"Public": "2k08L34Rcu+GgE7IeI9csPDcIC9Xu2ZGoD8Iv68EH1k=",
+		"Type": "Client"
+	}, {
+		"Client": 1,
+		"Private": "uBDccQqh0EvBIgdawRnFtLaai04bJZutaCAI/ZTOZEo=",
+		"Public": "ERsrxvhJVywVMXWJWtCAa0XmDw/0R1BPYPyosSImCTY=",
+		"Type": "Client"
+	}, {
+		"Client": 2,
+		"Private": "+IFBiFBWsY92lzx6UVlPc71ubxx/IKBp3d0uZ39cpVI=",
+		"Public": "ZODrhOIP8eyPr1gsR/zNk/vhW3HbmeJtiRtDF81+IU4=",
+		"Type": "Client"
+	}],
+	"Message": "Wireguard keys generated"
 }
 ```
--- app.py
+++ app.py
@@ -1,26 +1,36 @@
-from flask import Flask
+from flask import Flask, request
 import wgtools
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    public_key, private_key = wgtools.keypair()
-    if public_key is not None and private_key is not None:
-        result = {
-        "Code" : 200,
-        "Message" : "Wireguard key generated",
-        "Data": {
-            "Private": private_key,
-            "Public": public_key
-        },
-        }
+def keys():
+    key_list = []
+
+    if request.args.get('clients') is not None:
+        client_count = int(request.args.get('clients'))
     else:
-        result = {
-        "Code" : 203,
-        "Message" : "No content",
-        "Data": None
-        }
+        client_count = 1
+    
+    if request.args.get('servers') is not None:
+        server_count = int(request.args.get('servers'))
+    else:
+        server_count = 1
+    
+    for i in range(0,server_count):
+        public_key, private_key = wgtools.keypair()
+        key_list.append({"Type": "Server", "Server": i, "Private": private_key, "Public": public_key })
+
+    for i in range(0,client_count):
+        public_key, private_key = wgtools.keypair()
+        key_list.append({"Type": "Client", "Client": i, "Private": private_key, "Public": public_key })
+    
+    result = {
+    "Code" : 200,
+    "Message" : "Wireguard keys generated",
+    "Data": key_list,
+    }
+
     return result
 
 
