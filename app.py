c951936189e2be86b517d52e42950ff6f82552ae
Mike van den Brink <mbrink1999@outlook.com>
2020-11-03 19:41:30 +0100

    Generate config in base64 for server

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 import wgtools
+import base64
 
 app = Flask(__name__)
 
@@ -19,7 +20,10 @@ def keys():
     
     for i in range(0,server_count):
         public_key, private_key = wgtools.keypair()
-        key_list.append({"Type": "Server", "Server": i, "Private": private_key, "Public": public_key })
+        byte_string = generate_server_config(priv=private_key,pub=public_key).encode('ascii')
+        base64_bytes = base64.b64encode(byte_string)
+        config = base64_bytes.decode('ascii')
+        key_list.append({"Type": "Server", "Server": i, "Private": private_key, "Public": public_key, "Config": config})
 
     for i in range(0,client_count):
         public_key, private_key = wgtools.keypair()
@@ -33,6 +37,18 @@ def keys():
 
     return result
 
+def generate_server_config(priv,pub):
+    config = "[Interface]\n"
+    config += "Address = 10.0.0.1/24\n"
+    config += "ListenPort = 51820\n"
+    config += "PrivateKey = " + priv + "\n"
+    config += "PostUp = iptables -A FORWARD -i %i -j ACCEPT; iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE\n"
+    config += "PostDown = iptables -D FORWARD -i %i -j ACCEPT; iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE\n"
+    config += "\n"
+    config += "[Peer]\n"
+    config += "PublicKey = " + pub + "\n"
+    config += "AllowedIPs = 10.0.0.0/24\n"
+    return config
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=80)
